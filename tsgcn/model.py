6b78d76e005c1ae2f777baea367910508247e2ce
Murillo Rodrigues <murillofer.rodrigues@gmail.com>
2023-05-11 17:25:01 +0000

    viz embeds

--- tsgcn/model.py
+++ tsgcn/model.py
@@ -21,37 +21,38 @@ class BiGCNEncoder(torch.nn.Module):
         # Blowing up the input features dimension to self.out_features
         self.lin1 = Linear(self.in_features, self.out_features)
         # Convolution layers for forward and backward passes
-        # self.conv = GCNConv(self.in_features, self.out_features)
+        self.conv = GCNConv(self.out_features, self.out_features)
         self.conv_f = GCNConv(self.out_features, self.out_features)
         self.conv_b = GCNConv(self.out_features, self.out_features)
-        # self.batch_norm = BatchNorm1d(self.out_features, momentum=0.1)
-        # self.batch_norm_f = BatchNorm1d(self.out_features, momentum=0.1)
-        # self.batch_norm_b = BatchNorm1d(self.out_features, momentum=0.1)
+        self.batch_norm = BatchNorm1d(self.out_features, momentum=0.1)
+        self.batch_norm_f = BatchNorm1d(self.out_features, momentum=0.1)
+        self.batch_norm_b = BatchNorm1d(self.out_features, momentum=0.1)
         # Linear layer to combine the forward and backward embeddings
-        self.lin2 = Linear(self.out_features * 2, self.out_features)
+        self.lin2 = Linear(self.out_features * 3, self.out_features)
 
     def forward(self, data):
-        # x_a = data.x.clone()
+        x_a = data.x.clone()
         x_f = data.x.clone()
         x_b = data.x.clone()
         x_f = self.lin1(x_f)
         x_b = self.lin1(x_b)
+        x_a = self.lin1(x_a)
         for i in range(len(self.breaks) - 1):
             left = self.breaks[i]
             right = self.breaks[i + 1]
             subgraph_edge = data.get_subgraph(left, right)
             x_f = self.conv_f(x_f, subgraph_edge)
+            x_f = self.batch_norm_f(x_f)
         for i in range(len(self.breaks) - 1, 0, -1):
             left = self.breaks[i - 1]
             right = self.breaks[i]
             # print(left, right)
             subgraph_edge = data.get_subgraph(left, right)
             x_b = self.conv_b(x_b, subgraph_edge)
-        # x_a = self.conv(x_a, data.edge_index)
-        # x_a = self.batch_norm(x_a)
-        # x_f = self.batch_norm_f(x_f)
-        # x_b = self.batch_norm_b(x_b)
-        x = torch.concat((x_f, x_b), 1)
+            x_b = self.batch_norm_b(x_b)
+        x_a = self.conv(x_a, data.edge_index)
+        x_a = self.batch_norm(x_a)
+        x = torch.concat((x_a, x_f, x_b), 1)
         x = self.lin2(x)
         return x
 
